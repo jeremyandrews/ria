c8c3ea22614e9bcc6aa23d1fc3f666df121dffda
Jeremy Andrews <jeremy@tag1consulting.com>
2022-11-08 15:58:28 +0100

    inline docs

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,7 @@ impl FromStr for MediaType {
     }
 }
 
+/// Skip files and directories that start with ".".
 #[instrument]
 fn is_hidden(entry: &DirEntry) -> bool {
     event!(Level::TRACE, "is_hidden");
@@ -55,22 +56,77 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
+/// Convert audio file tag to String for specific tags that we care about.
 #[instrument]
 fn get_tag_value(t: &str, v: &glib::SendValue) -> Option<String> {
     event!(Level::TRACE, "get_tag_value");
 
+    // This list was derived from scanning 100,000 audio files and looking at the contained
+    // tags:
+    // {
+    //   "acoustid-id": 317,
+    //   "album": 64799,
+    //   "album-artist": 41890,
+    //   "album-artist-sortname": 622,
+    //   "album-disc-count": 23581,
+    //   "album-disc-number": 29428,
+    //   "album-sortname": 30,
+    //   "application-name": 7861,
+    //   "artist": 64853,
+    //   "artist-sortname": 778,
+    //   "audio-codec": 66659,
+    //   "beats-per-minute": 815,
+    //   "bitrate": 113,
+    //   "chromaprint-fingerprint": 13,
+    //   "comment": 19259,
+    //   "composer": 9995,
+    //   "contact": 115,
+    //   "copyright": 10761,
+    //   "datetime": 60746,
+    //   "description": 4206,
+    //   "discid": 721,
+    //   "extended-comment": 27470,
+    //   "genre": 55359,
+    //   "geo-location-name": 125,
+    //   "image": 30422,
+    //   "isrc": 9984,
+    //   "language-code": 873,
+    //   "maximum-bitrate": 111,
+    //   "minimum-bitrate": 111,
+    //   "musicbrainz-albumartistid": 1304,
+    //   "musicbrainz-albumid": 1354,
+    //   "musicbrainz-artistid": 1318,
+    //   "musicbrainz-discid": 668,
+    //   "musicbrainz-releasegroupid": 622,
+    //   "musicbrainz-releasetrackid": 512,
+    //   "musicbrainz-trackid": 1380,
+    //   "organization": 5319,
+    //   "performer": 1512,
+    //   "preview-image": 82,
+    //   "replaygain-album-gain": 2970,
+    //   "replaygain-album-peak": 2915,
+    //   "replaygain-reference-level": 631,
+    //   "replaygain-track-gain": 4986,
+    //   "replaygain-track-peak": 4883,
+    //   "title": 64485,
+    //   "title-sortname": 12,
+    //   "track-count": 35632,
+    //   "track-number": 64069,
+    //   "version": 191,
+    // }
+    // @TODO: extract images, explore other tags (such as comment).
     let tags_to_store = vec![
-        "audio-codec",
-        "track-number",
-        "datetime",
-        "artist",
         "album",
-        "title",
-        "genre",
         "album-artist",
-        "track-count",
         "album-disc-number",
         "album-disc-count",
+        "artist",
+        "audio-codec",
+        "datetime",
+        "genre",
+        "title",
+        "track-number",
+        "track-count",
     ];
     if tags_to_store.contains(&t) {
         if let Ok(s) = v.get::<&str>() {
@@ -88,7 +144,7 @@ fn get_tag_value(t: &str, v: &glib::SendValue) -> Option<String> {
 #[tokio::main]
 async fn main() -> Result<(), ()> {
     // Display INFO and higher level logs.
-    // @TODO: Make this configurable.
+    // @TODO: Make this configurable/dynamic.
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
         .finish();
@@ -340,7 +396,7 @@ async fn main() -> Result<(), ()> {
                     // @TODO: Detect changes to the files, and update as needed.
                     // @TODO: Error handling.
                     if existing.is_none() {
-                        event!(Level::INFO, "Insert Audio File: {:?}", audio);
+                        event!(Level::DEBUG, "Insert Audio File: {:?}", audio);
                         let new_audio = Audio::insert(audio)
                             .exec(&db)
                             .await
@@ -357,6 +413,7 @@ async fn main() -> Result<(), ()> {
                                             value: ActiveValue::Set(s),
                                             ..Default::default()
                                         };
+                                        event!(Level::DEBUG, "Insert Audiotag: {:?}", tag);
                                         Audiotag::insert(tag)
                                             .exec(&db)
                                             .await
@@ -392,8 +449,8 @@ async fn main() -> Result<(), ()> {
             // auto-identifying albums.
         }
 
-        // @TODO: Make this optional/configurable.
-        if counter > 10_000 {
+        // @TODO: Make this configurable and optional.
+        if counter > 25_000 {
             break;
         }
     }
